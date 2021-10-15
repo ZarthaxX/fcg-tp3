cf3b708e3309aad8de26d7e332053f219684d796
ZarthaxX <tomastropea@hotmail.com>
2021-10-14 22:11:04 -0300

    excercise done

--- ejercicio.js
+++ ejercicio.js
@@ -6,66 +6,102 @@ class CurveDrawer
 	constructor()
 	{
 		// Creamos el programa webgl con los shaders para los segmentos de recta
-		this.prog   = InitShaderProgram( curvesVS, curvesFS );
+		this.prog = InitShaderProgram(curvesVS, curvesFS);
+		
+		this.cp = [0,0,0,0];
+		this.mvp = gl.getUniformLocation(this.prog, 'mvp');
+		this.t = gl.getAttribLocation(this.prog, 't');
+		this.cp[0] = gl.getUniformLocation(this.prog, 'cp0');
+		this.cp[1] = gl.getUniformLocation(this.prog, 'cp1');
+		this.cp[2] = gl.getUniformLocation(this.prog, 'cp2');
+		this.cp[3] = gl.getUniformLocation(this.prog, 'cp3');
 
-		// [Completar] Incialización y obtención de las ubicaciones de los atributos y variables uniformes
-				
 		// Muestreo del parámetro t
 		this.steps = 100;
 		var tv = [];
-		for ( var i=0; i<this.steps; ++i ) {
-			tv.push( i / (this.steps-1) );
+		for (let i = 0; i < this.steps; i++ ) {
+			tv.push(i / (this.steps - 1) );
 		}
 		
-		// [Completar] Creacion del vertex buffer y seteo de contenido
+		this.buffer = gl.createBuffer();
+		// Enviamos al buffer
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
+		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(tv), gl.STATIC_DRAW);
 	}
 
 	// Actualización del viewport (se llama al inicializar la web o al cambiar el tamaño de la pantalla)
 	setViewport( width, height )
 	{
-		// [Completar] Matriz de transformación.
-		// [Completar] Binding del programa y seteo de la variable uniforme para la matriz. 
+		var trans = [ 
+			2 / width, 0, 0, 0,
+			0, -2 / height, 0, 0,
+			0, 0, 1, 0,
+			-1, 1, 0, 1 
+		];
+
+		gl.useProgram(this.prog);
+		gl.uniformMatrix4fv(this.mvp, false, trans);
 	}
 
 	updatePoints( pt )
 	{
-		// [Completar] Actualización de las variables uniformes para los puntos de control
-		// [Completar] No se olviden de hacer el binding del programa antes de setear las variables 
-		// [Completar] Pueden acceder a las coordenadas de los puntos de control consultando el arreglo pt[]:
-		// var x = pt[i].getAttribute("cx");
-		// var y = pt[i].getAttribute("cy");
+		gl.useProgram( this.prog );
+		
+		for (let i = 0; i < 4; i++) {
+			var x = pt[i].getAttribute("cx");
+			var y = pt[i].getAttribute("cy");
+			gl.uniform2f(this.cp[i], x, y);
+		}
 	}
 
 	draw()
 	{
-		// [Completar] Dibujamos la curva como una LINE_STRIP
-		// [Completar] No se olviden de hacer el binding del programa y de habilitar los atributos de los vértices
+		// Seleccionamos el shader
+		gl.useProgram(this.prog);
+		
+		// Binding del buffer de los parametros de la curva
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
+		// Habilitamos el atributo 
+		gl.vertexAttribPointer(this.t, 1, gl.FLOAT, false, 0, 0);
+		gl.enableVertexAttribArray(this.t);
+		
+		// Dibujamos lineas utilizando primitivas gl.LINE_STRIP 
+		gl.drawArrays(gl.LINE_STRIP, 0, this.steps);
 	}
 }
 
 // Vertex Shader
-//[Completar] El vertex shader se ejecuta una vez por cada punto en mi curva (parámetro step). No confundir punto con punto de control.
-// Deberán completar con la definición de una Bezier Cúbica para un punto t. Algunas consideraciones generales respecto a GLSL: si
-// declarás las variables pero no las usás, no se les asigna espacio. Siempre poner ; al finalizar las sentencias. Las constantes
-// en punto flotante necesitan ser expresadas como X.Y, incluso si son enteros: ejemplo, para 4 escribimos 4.0
 var curvesVS = `
 	attribute float t;
 	uniform mat4 mvp;
-	uniform vec2 p0;
-	uniform vec2 p1;
-	uniform vec2 p2;
-	uniform vec2 p3;
+	uniform vec2 cp0;
+	uniform vec2 cp1;
+	uniform vec2 cp2;
+	uniform vec2 cp3;
+	varying float tcolor;
+
+	vec2 bezierCurve() {
+		return 
+			pow(1.0 - t, 3.0) * cp0 +
+			3.0 * pow(1.0 - t, 2.0) * t * cp1 +
+			3.0 * (1.0 - t) * pow(t, 2.0) * cp2 +
+			pow(t, 3.0) * cp3;
+	}
+
 	void main()
 	{ 
-		gl_Position = vec4(0,0,0,1);
+		vec2 cubicBezier = bezierCurve();
+		gl_Position =  mvp * vec4(cubicBezier, 0.0, 1.0);
+		tcolor = t;
 	}
 `;
 
 // Fragment Shader
 var curvesFS = `
 	precision mediump float;
+	varying float tcolor;
 	void main()
 	{
-		gl_FragColor = vec4(0,0,1,1);
+		gl_FragColor = vec4(tcolor * tcolor, tcolor, 1.0 - tcolor, 1.0);
 	}
 `;
